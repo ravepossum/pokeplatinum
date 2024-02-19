7192ea923ac00b355cffd7580e61f8933c4fe52a
ravepossum <ravepossum@example.com>
2024-02-18 20:00:16 -0500

    can forget HMs, invert running, remove build tests

--- src/unk_0205F180.c
+++ src/unk_0205F180.c
@@ -2220,10 +2220,10 @@ static int sub_020613D8 (int param0)
 int sub_020613FC (UnkStruct_0205E884 * param0, u16 param1)
 {
     if ((param1 & PAD_BUTTON_B)) {
-        return 1;
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
 
 int sub_0206140C (UnkStruct_0205E884 * param0)
