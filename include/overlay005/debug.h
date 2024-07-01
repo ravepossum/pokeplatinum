35b14e467eb204b5312ddc622f2721950fd39034
RavePossum <ravepossum@proton.me>
2024-07-01 14:47:31 -0400

    Implement mon debug menu, plus minor clean-up

--- include/overlay005/debug.h
+++ include/overlay005/debug.h
@@ -5,7 +5,14 @@
 #include "struct_decls/struct_0200112C_decl.h"
 #include "struct_defs/struct_0205AA50.h"
 #include "struct_decls/struct_02013A04_decl.h"
+#include "struct_decls/struct_020149F0_decl.h"
 #include "sys_task_manager.h"
+#include "string_template.h"
+#include "message.h"
+
+#define DEBUG_KEY 					PAD_BUTTON_R
+#define DEBUG_MENU_DUMMY_FUNCTION 	0xFFFFFFFD
+#define DEBUG_MENU_MESSAGE_BANK 	328
 
 typedef struct DebugMenu {
 	u8 data;
@@ -15,7 +22,7 @@ typedef struct DebugMenu {
 	FieldSystem *sys;
 	Window *window;
 	ResourceMetadata *menuList;
-	void (*callback)(FieldSystem *);
+	void (*callback)(FieldSystem*);
 } DebugMenu;
 
 typedef struct DebugMenuItem {
@@ -23,21 +30,19 @@ typedef struct DebugMenuItem {
 	u32	function;
 } DebugMenuItem;
 
+enum DebugItems {
+	DEBUG_HEADER = 1,
+	DEBUG_FLY,
+	DEBUG_GIVE_MON
+};
+
 typedef struct DebugFly {
-	FieldSystem* sys;
-	void* data;
-	SysTask* taskCutIn;
+	FieldSystem *sys;
+	void *data;
+	SysTask *taskCutIn;
 	int	sequence;
 	int	wipeFunc;
 } DebugFly;
-
-enum DebugItems {
-	DEBUG_HEADER = 1,
-	DEBUG_FLY
-};
-
-#define DEBUG_KEY 					PAD_BUTTON_R
-#define DEBUG_MENU_DUMMY_FUNCTION 	0xFFFFFFFD
 
 void DebugMenu_Init(FieldSystem *sys);
 
