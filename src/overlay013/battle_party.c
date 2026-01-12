0a14829659c2afaa3f21f37cb4a481d5adc81577
RavePossum <ravepossum@proton.me>
2026-01-11 21:19:52 -0700

    Allow deleting HM moves

--- src/overlay013/battle_party.c
+++ src/overlay013/battle_party.c
@@ -715,19 +715,7 @@ static u8 BattlePartyTask_ConfirmLearnMoveScreen(BattleParty *battleParty)
             ov13_02225FCC(battleParty, 29);
         }
 
-        if (CheckSelectedMoveIsHM(battleParty) == TRUE) {
-            BattlePartyText_PrintHMMovesCantBeForgottenText(battleParty);
-
-            if (battleParty->displayingContestStats == FALSE) {
-                ClearMoveStats(battleParty);
-            } else {
-                ClearMoveContestStats(battleParty);
-            }
-
-            battleParty->queuedState = TASK_STATE_CONFIRM_LEARN_MOVE_SCREEN;
-        } else {
-            battleParty->queuedState = TASK_STATE_EXIT;
-        }
+        battleParty->queuedState = TASK_STATE_EXIT;
 
         return TASK_STATE_SCREEN_TRANSITION;
     case BATTLE_CONFIRM_LEARN_MOVE_SCREEN_BUTTON_CONTEST_STATS:
